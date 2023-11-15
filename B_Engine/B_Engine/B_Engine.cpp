af5cb5ffa48ac4474c5a9b9f9fede2685830de54
Shin.H.S <guigak136@tukorea.ac.kr>
2023-11-16 00:47:33 +0900

    Add function about command
    add function for processing command on console
    command is string

--- B_Engine/B_Engine/B_Engine.cpp
+++ B_Engine/B_Engine/B_Engine.cpp
@@ -24,7 +24,7 @@ INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 CFramework gFramework;
 
 // console function
-void Prcs_Console_Msg(char* sMessage);
+void Prcs_Console_Cmd();
 
 DWORD WINAPI Prcs_Console(LPVOID argument) {
     AllocConsole();
@@ -37,19 +37,9 @@ DWORD WINAPI Prcs_Console(LPVOID argument) {
     _tsetlocale(LC_ALL, _T(""));
 
     //
-    int num = 0;
+    Prcs_Console_Cmd();
 
-    while (1) {
-        scanf("%d", &num);
-        printf("%d", num);
-
-        if (num == -1) {
-            g_bConsole = false;
-            g_bActive = false;
-            break;
-        }
-    }
-
+    //
     HWND hWndConsole = GetConsoleWindow();
     ShowWindow(hWndConsole, SW_HIDE);
 
@@ -70,7 +60,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     // TODO: 여기에 코드를 입력합니다.
 
     // TODO: 테스트용 나중에 지우기, 2019180031 - 서버에 연결
-    Connect_To_Server((char*)("127.0.0.1"));
+    //Connect_To_Server((char*)("127.0.0.1"));
 
     // 전역 문자열을 초기화합니다.
     LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
@@ -278,6 +268,24 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 
 
 // console function
-void Prcs_Console_Msg(char* sMessage) {
+void Prcs_Console_Cmd() {
+    char cmd[10] = { 0 };
 
+    while (1) {
+        printf("\n커맨드를 입력해주세요.\n");
+        printf("커맨드 창 종료 - exit\n");
+        printf("프로그램 종료 - quit\n");
+
+        scanf_s("%s", cmd, (int)sizeof(cmd));
+
+        if (!strcmp(cmd, "exit")) {
+            g_bConsole = false;
+            break;
+        }
+        else if (!strcmp(cmd, "quit")) {
+            g_bConsole = false;
+            g_bActive = false;
+            break;
+        }
+    }
 }
